1080193b019aac8e63e7265246720adcda0c6288
fable.wu <fable.wu@example.com>
2021-10-09 17:12:12 +0800

    字面值常量

--- Chapter_2_Variables and basic types/variable_basic_types/variable_basic_types.cpp
+++ Chapter_2_Variables and basic types/variable_basic_types/variable_basic_types.cpp
@@ -21,11 +21,34 @@ void test_2_1_2() {
 	cout << u + i2 << endl; // 如果int占32位，输出4294967264
 }
 
+void test_2_1_3() {
+	//字面值常量
+	20; /*十进制*/
+	024; /*八进制*/
+	0x14; /*十六进制*/
+	cout << "2e2: " << 2e2 << endl;
+	3.14159;
+	cout << "3.14159E0: " << 3.14159E0 << endl;
+	'a'; //字符字面值
+	"Hello World"; //字符串字面值
+	cout << "\tHi! \n";
+	//指定字面值的类型
+	L'a'; //宽字符型字面值，类型是wchar_t
+	u8"hi!"; //utf-8字符串字面值
+	42ULL;// unsigned long long
+	1E-3F; // float
+	3.14159L;// long double
+	//布尔字面值和指针字面值
+	bool test = false; // true和false是布尔类型的字面值
+	nullptr; //指针字面值
+}
+
 
 
 int main()
 {
 	cout << "Hello variable and basic_types !" << endl;
 	test_2_1_2();
+	test_2_1_3();
 	return 0;
 }
