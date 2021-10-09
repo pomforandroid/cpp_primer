2bf0e6d7d9546f85cfc807364a459e4510ee6606
fable.wu <fable.wu@example.com>
2021-10-09 16:31:04 +0800

    基本内置类型，类型转换

---
+++ Chapter_2_Variables and basic types/variable_basic_types/variable_basic_types.cpp
@@ -0,0 +1,31 @@
+﻿// variable_basic_types.cpp : Defines the entry point for the application.
+//
+
+#include "variable_basic_types.h"
+
+using namespace std;
+
+void test_2_1_2() {
+	bool b = 42; // b为真
+	int i = b; // i的值为1
+	i = 3.14; //i的值为3
+	double pi = i; // pi的值为3.0
+	unsigned char c = -1;; // 假设char占8比特，c的值为255
+	signed char c2 = 256; // 假设char占8比特，c2的值是未定义的
+	cout << "c: " << c << " c2: " << c2 << endl; // 输出空白，why? 后面找到原因了更新
+
+	//含有无符号类型的表达式
+	unsigned u = 10;
+	int i2 = -42;
+	cout << i2 + i2 << endl; // 输出-84
+	cout << u + i2 << endl; // 如果int占32位，输出4294967264
+}
+
+
+
+int main()
+{
+	cout << "Hello variable and basic_types !" << endl;
+	test_2_1_2();
+	return 0;
+}
