38d515fc6536a1d44d4bff8359a7389b93990cc1
fable.wu <fable.wu@example.com>
2021-10-19 13:38:33 +0800

    泛型算法概述

--- Chapter_10_Generic_Algorithm/generic_algorithm/generic_algorithm.cpp
+++ Chapter_10_Generic_Algorithm/generic_algorithm/generic_algorithm.cpp
@@ -2,11 +2,36 @@
 //
 
 #include "generic_algorithm.h"
+#include <list>
+#include <vector>
 
 using namespace std;
 
+void test_10_1() {
+	//概述
+	int val = 42;
+	vector<int> vec{ 1, 2, 3, 4, 5 };
+
+	auto result = find(vec.cbegin(), vec.cend(), val);
+
+	//report result
+	cout << "the value " << val
+		<< (result == vec.cend() ?
+			"is no present" : "is present") << endl;
+
+	string val2 = "a value";// the value which we want to find
+	list<string> lst;
+	auto result2 = find(lst.cbegin(), lst.cend(), val2);
+
+	int ia[] = { 27, 210, 12, 47, 109, 83 };
+	int val3 = 83;
+	int* result3 = find(begin(ia), end(ia), val3);
+	cout << "result " << *result3 << endl;
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << "Hello 泛型算法." << endl;
+	test_10_1();
 	return 0;
 }
