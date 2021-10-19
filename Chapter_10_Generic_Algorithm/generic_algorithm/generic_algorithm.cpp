c78f834d98791ef2d5ef45d091f8d43a146fc6e2
fable.wu <fable.wu@example.com>
2021-10-19 13:28:53 +0800

    init 泛型算法

---
+++ Chapter_10_Generic_Algorithm/generic_algorithm/generic_algorithm.cpp
@@ -0,0 +1,12 @@
+﻿// generic_algorithm.cpp : Defines the entry point for the application.
+//
+
+#include "generic_algorithm.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
