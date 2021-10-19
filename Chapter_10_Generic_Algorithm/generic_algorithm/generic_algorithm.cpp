059921a0168cd8dabb0d5b63d4b8b781bbc832fb
fable.wu <fable.wu@example.com>
2021-10-19 14:06:36 +0800

    写容器元素的算法

--- Chapter_10_Generic_Algorithm/generic_algorithm/generic_algorithm.cpp
+++ Chapter_10_Generic_Algorithm/generic_algorithm/generic_algorithm.cpp
@@ -52,10 +52,40 @@ void test_10_2_1() {
 
 }
 
+void test_10_2_2() {
+	//写容器元素的算法
+	vector<int> ivec = { 1, 2, 3 };
+	fill_n(ivec.begin(), ivec.size(), 0);
+
+	//back+inserter
+	vector <int> ivec2;
+	auto it = back_inserter(ivec2); //通过它赋值会将元素添加到vec中
+	*it = 42;
+	for (auto a : ivec2)
+		cout << a << endl;
+
+	vector<int> ivec3;
+
+	fill_n(back_inserter(ivec3), 10, 0);
+
+	//拷贝算法copy
+
+	int a1[] = { 0,1,2,3,4,5,6,7,8,9 };
+	int a2[sizeof(a1) / sizeof(*a1)]; // a2与a1的大小一样
+	//ret指向拷贝到a2的尾元素之后的位置
+	auto ret = copy(begin(a1), end(a1), a2);// 把a1的内容拷贝给a2
+	//re指向拷贝到a2的尾元素之后的位置
+	cout << "copy a1 to a2" << endl;
+	for (auto a : a2)
+		cout << a << endl;
+
+}
+
 int main()
 {
 	cout << "Hello 泛型算法." << endl;
 	test_10_1();
 	test_10_2_1();
+	test_10_2_2();
 	return 0;
 }
