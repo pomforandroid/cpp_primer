1a8911014fffa1c11381a4f976d52d299b6e7d69
fable.wu <fable.wu@example.com>
2021-11-05 17:30:19 +0800

    动态内存与智能指针 init

---
+++ Chapter_12_Dynamic_memory_and_smart_pointers/Dynamic_memory_and_samrt_points/Dynamic_memory_and_samrt_points.cpp
@@ -0,0 +1,16 @@
+﻿// Dynamic_memory_and_samrt_points.cpp : Defines the entry point for the application.
+//
+
+#include "Dynamic_memory_and_samrt_points.h"
+#include <list>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Chapter_12_Dynamic_memory_and_smart_pointers/Dynamic_memory_and_samrt_points/Dynamic_memory_and_samrt_points.h
@@ -0,0 +1,8 @@
+﻿// Dynamic_memory_and_samrt_points.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
