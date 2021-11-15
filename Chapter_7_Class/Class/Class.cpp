0a81cd674dadfcff5a6b9259e5f8c3ea888e42b6
fable.wu <fable.wu@example.com>
2021-11-15 13:51:58 +0800

    定义类相关的非成员函数

--- Chapter_7_Class/Class/Class.cpp
+++ Chapter_7_Class/Class/Class.cpp
@@ -44,6 +44,30 @@ Sales_data& Sales_data::combine(const Sales_data& rhs) {
 	return *this;
 }
 
+// Define class-related non-member functions
+istream& read(istream& is, Sales_data& item) {
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
+
+ostream& print(ostream& os, const Sales_data& item) {
+	os << item.isbn() << " " << item.bookNo << ""
+		<< item.revenue << " " << item.avg_price();
+	return os;
+}
+
+Sales_data add(const Sales_data& lhs, const Sales_data&rhs) {
+	Sales_data sum = lhs; // copy the data of member if lhs to sum
+	sum.combine(rhs);
+	return sum;
+}
+
+
+
+
+
 void use_improved_Sales_data(){
 	Sales_data total;
 	if (read(cin, total)) {
